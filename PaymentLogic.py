c6498876d9fa225f77060bfaae73110a4e9d51e1
jahlonteaching <jahlon@outlook.com>
2024-02-13 09:34:42 -0500

    Version inicial

---
+++ PaymentLogic.py
@@ -0,0 +1,14 @@
+
+
+def calcPayment(purchase_amount,interest_rate,num_payments):
+    """
+    Calcula la cuota a pagar por una compra con una tarjeta de crédito
+    compra : Valor de la compra con la tarjeta
+    tasa : Debe ser un porcentaje entre 1 y 100
+    plazo : numero de cuotas a diferir la compra
+
+    El resultado no esta redondeado
+    """
+    i = interest_rate
+    return (purchase_amount * i) / (1 - (1 + i) ** (-num_payments))
+
